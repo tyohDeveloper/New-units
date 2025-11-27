2a24a8a709f148b7758d6966c7029799891f8f5b
tyohDeveloper <1989102-tyohDeveloper@users.noreply.replit.com>
2025-11-27 18:59:26 +0000

    Restored to '87feb08ade1d850f1bb162e5137b28c4f72b8ac2'
    Replit-Restored-To: 87feb08ade1d850f1bb162e5137b28c4f72b8ac2

--- server/index-dev.ts
+++ server/index-dev.ts
@@ -2,27 +2,64 @@ import fs from "node:fs";
 import { type Server } from "node:http";
 import path from "node:path";
 
-import express, { type Express } from "express";
+import type { Express } from "express";
+import { nanoid } from "nanoid";
+import { createServer as createViteServer, createLogger } from "vite";
 
 import runApp from "./app";
 
-export async function serveStatic(app: Express, server: Server) {
-  const distPath = path.resolve(import.meta.dirname, "..", "dist", "public");
+import viteConfig from "../vite.config";
 
-  if (!fs.existsSync(distPath)) {
-    throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to run 'npm run build' first`,
-    );
-  }
+const viteLogger = createLogger();
 
-  app.use(express.static(distPath));
+export async function setupVite(app: Express, server: Server) {
+  const serverOptions = {
+    middlewareMode: true,
+    hmr: { server },
+    allowedHosts: true as const,
+  };
 
-  // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  const vite = await createViteServer({
+    ...viteConfig,
+    configFile: false,
+    customLogger: {
+      ...viteLogger,
+      error: (msg, options) => {
+        viteLogger.error(msg, options);
+        process.exit(1);
+      },
+    },
+    server: serverOptions,
+    appType: "custom",
+  });
+
+  app.use(vite.middlewares);
+  app.use("*", async (req, res, next) => {
+    const url = req.originalUrl;
+
+    try {
+      const clientTemplate = path.resolve(
+        import.meta.dirname,
+        "..",
+        "client",
+        "index.html",
+      );
+
+      // always reload the index.html file from disk incase it changes
+      let template = await fs.promises.readFile(clientTemplate, "utf-8");
+      template = template.replace(
+        `src="/src/main.tsx"`,
+        `src="/src/main.tsx?v=${nanoid()}"`,
+      );
+      const page = await vite.transformIndexHtml(url, template);
+      res.status(200).set({ "Content-Type": "text/html" }).end(page);
+    } catch (e) {
+      vite.ssrFixStacktrace(e as Error);
+      next(e);
+    }
   });
 }
 
 (async () => {
-  await runApp(serveStatic);
+  await runApp(setupVite);
 })();
